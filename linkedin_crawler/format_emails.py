42a36a8b33119ae6229f8357360d8f8673300a8b
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2024-09-30 17:14:04 +0200

    Make stripping maiden name optional

--- linkedin_crawler/format_emails.py
+++ linkedin_crawler/format_emails.py
@@ -17,17 +17,20 @@ class NoNameException(Exception):
     pass
 
 
-def get_email(email_format, domain, name):
+def get_email(email_format, domain, name, strip_maiden_name=True):
     names = split_name(name)
     # Remove other initials
     names = [x for x in names if '.' not in x]
     if not names:
         raise NoNameException()
     # Strip maiden name
-    names[-1] = names[-1].split('-')[0]
+    if strip_maiden_name:
+        names[-1] = names[-1].split('-')[0]
 
     name = email_formats[email_format](names)
-    name = name.lower().replace("ö", "oe").replace("ü", "ue").replace("ä", "ae").replace("-",'')
+    name = name.lower().replace("ö", "oe").replace("ü", "ue").replace("ä", "ae")
+    if strip_maiden_name:
+        name = name.replace('-','')
     name = re.sub(r'[àáâãå]', 'a', name)
     name = re.sub(r'[èéêë]', 'e', name)
     name = re.sub(r'[ìíîï]', 'i', name)
@@ -45,6 +48,7 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output-file", default="emails.txt")
     parser.add_argument("-d", "--domain", required=True)
     parser.add_argument("-f", "--email-format", default="first.last", choices=email_formats.keys())
+    parser.add_argument("--no-strip-maiden-name", action="store_true", help="Do not strip the maiden name")
     args = parser.parse_args()
 
     with open(args.csv_file) as f, open(args.output_file, 'w+') as f1:
@@ -52,5 +56,5 @@ if __name__ == "__main__":
         writer = csv.DictWriter(f1, reader.fieldnames)
         writer.writeheader()
         for row in reader:
-            row['email'] = get_email(args.email_format, args.domain, row['name'])
+            row['email'] = get_email(args.email_format, args.domain, row['name'], strip_maiden_name=not args.no_strip_maiden_name)
             writer.writerow(row)
